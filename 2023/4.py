9c8d4398c972360b8783d038396899adc58d9315
Jaysheel Dodia <73154873+jaysheel-ops@users.noreply.github.com>
2023-12-04 14:27:03 +0530

    update on both solutions

--- 2023/4.py
+++ 2023/4.py
@@ -7,7 +7,7 @@ s2 = 0
 copies = {}
 total_cards = len(D.split("\n"))
 cardno = 1
-print(total_cards)
+
 for no in range(total_cards):
     copies[no+1] =  1
 for line in D.split("\n"):
@@ -18,36 +18,32 @@ for line in D.split("\n"):
     your = [int(c) for c in your.split()]
     
     tempsum = 0
+    for i in winning: 
+        if i in your:
+            if tempsum == 0:
+                tempsum=1
+            else:
+                tempsum*=2
+
+    s1 += tempsum
+
+    tempsum = 0
     for i in your:
         if i in winning:
             tempsum+=1
-            print('tempsum', tempsum)
-    
-    print(cardno)
+
     for i in range(cardno+1, 1+cardno+tempsum):
-        if cardno == 1:
-            print('copies of 1', copies[i])
         copies[i] += copies[cardno] 
-        if cardno == 4:
-            print('copies of 4 ', copies[i])
         
     cardno+=1
 
-
+print(s1)
     
 for key, value in copies.items():
-    print("Key ", key, "value ", value)
+
     s2+=value
 print(s2)
     # s1
-    # for i in winning: 
-    #     if i in your:
-    #         if tempsum == 0:
-    #             tempsum=1
-    #         else:
-    #             tempsum*=2
-
-    # s1 += tempsum
 
 
 
